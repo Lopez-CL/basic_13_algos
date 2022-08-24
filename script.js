5b8e21140ddabc97ddb2103c6d133736a9096b09
Lopez-CL <cllopez511@gmail.com>
2022-08-24 07:06:35 -0400

    adding algo challenges 4-6

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function printOdds1To255(){
     return holdNum;
 }
 console.log(printOdds1To255());
-
+//algo3//
 function printIntsAndSum0To255(){
     var sum = 0;
     for (var i = 0; i < 256; i++){
@@ -31,3 +31,36 @@ function printIntsAndSum0To255(){
     return "The final sum is " + sum;
 }
 console.log(printIntsAndSum0To255());
+
+//algo 4//
+
+function printArrayVals(arr){
+    for (var i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
+}
+
+console.log(printArrayVals([1, 2, 3, 5, 23, 20, -2, 4, 5]));
+
+function printMaxOfArray(arr){
+    var max = 0;
+    for (var i = 0; i < arr.length; i++){
+        if (max < arr[i]){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(printMaxOfArray([1, 2, 3, 5, 23, 20, -2, 4, 5]));
+
+//algo 6//
+
+function printAverageOfArray(arr){
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+    }
+    sum = sum / arr.length;
+    return Math.round(sum);
+}
+console.log(printAverageOfArray([1, 2, 3, 5, 23, 20, 2, 4, 5]));
